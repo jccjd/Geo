8a411e342e6ab5d1528c5605d5480558cef86629
jccjd <1340944538@qq.com>
2017-10-06 00:13:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/myapplication/Question.java
@@ -0,0 +1,29 @@
+package com.example.lenovo.myapplication;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question (int textResId, boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+        mTextResId = textResId;
+
+    }
+
+}
