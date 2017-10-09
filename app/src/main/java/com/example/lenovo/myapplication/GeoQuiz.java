ba4221a102a250e5850ff50e58ca22aa1945cf1f
jccjd <1340944538@qq.com>
2017-10-10 00:35:15 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/GeoQuiz.java
+++ app/src/main/java/com/example/lenovo/myapplication/GeoQuiz.java
@@ -2,12 +2,34 @@ package com.example.lenovo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GeoQuiz extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_quiz);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        /*匿名内部类的两大好处
+        *   1.因为匿名内部类的可以在同一处实现监听器方法，代码清晰可读
+        *   2.事件监听器一般只在同一处使用，使匿名内部类可以避免不必要的命名类的实现
+        * */
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Does nothing yet,but soon!
+                Toast.makeText(GeoQuiz.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mTrueButton = (Button)findViewById(R.id.true_button);
     }
 }
