9bcbe8fdf0a1d7dec46f7692e829d439b0e94c3a
jccjd <1340944538@qq.com>
2017-10-15 17:15:21 +0800

    updata

--- app/src/main/java/com/example/lenovo/myapplication/CheatActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/CheatActivity.java
@@ -1,9 +1,18 @@
 package com.example.lenovo.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String EXTRA_ANSWER_IS__TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS__TRUE,answerIsTrue);
+        return i;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
